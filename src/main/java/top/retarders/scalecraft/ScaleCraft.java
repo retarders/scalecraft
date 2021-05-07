176b3d3ae7b4ff2738d1d916875444c23c1f7d5a
pcranaway <pcranaway@tuta.io>
2021-05-08 01:55:40 +0300

    add docker

--- src/main/java/top/retarders/scalecraft/ScaleCraft.java
+++ src/main/java/top/retarders/scalecraft/ScaleCraft.java
@@ -1,11 +1,20 @@
 package top.retarders.scalecraft;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class ScaleCraft extends Plugin {
 
     @Override
     public void onEnable() {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost("unix:///var/run/docker.sock")
+                .build();
+
+        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
     }
 
 }
