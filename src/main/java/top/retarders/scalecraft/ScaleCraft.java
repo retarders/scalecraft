45d7e924ff6b561e0163aba7bbc2838ea6d520e8
pcranaway <pcranaway@tuta.io>
2021-05-08 01:52:55 +0300

    initial

---
+++ src/main/java/top/retarders/scalecraft/ScaleCraft.java
@@ -0,0 +1,11 @@
+package top.retarders.scalecraft;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class ScaleCraft extends Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
